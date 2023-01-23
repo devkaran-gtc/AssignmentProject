c11d161c6e73019bc3f2bb940ff3b4b28c480a4a
App-developer007 <74863274+App-developer007@users.noreply.github.com>
2023-01-23 15:01:47 +0530

    Bug fixes (#7)
    * changes
    
    * UI changes

--- LoginSignupUI/LoginSignupUI/ViewControllers/ProfileViewController.swift
+++ LoginSignupUI/LoginSignupUI/ViewControllers/ProfileViewController.swift
@@ -62,22 +62,22 @@ class ProfileViewController: UIViewController {
         aboutLbl.text = about
         
 
-        downloadJSON {
-            self.profileName?.text = self.profile?.full_name
-            self.aboutLbl?.text = self.profile?.about_me
-            self.followers.text = "\((self.profile?.followers)!)"
-            self.following.text = "\((self.profile?.following)!)"
-            let imageurl = self.profile?.imageurl
-            let url = URL(string: imageurl!)
-            let data = try? Data(contentsOf: url!)
-            if let imageData = data {
-                let image = UIImage(data: imageData)
-                self.profileImgView.image = image
-            }else {
-                self.profileImgView.image = nil
-            }
-            print("Success")
-        }
+//        downloadJSON {
+//            self.profileName?.text = self.profile?.full_name
+//            self.aboutLbl?.text = self.profile?.about_me
+//            self.followers.text = "\((self.profile?.followers)!)"
+//            self.following.text = "\((self.profile?.following)!)"
+//            let imageurl = self.profile?.imageurl
+//            let url = URL(string: imageurl!)
+//            let data = try? Data(contentsOf: url!)
+//            if let imageData = data {
+//                let image = UIImage(data: imageData)
+//                self.profileImgView.image = image
+//            }else {
+//                self.profileImgView.image = nil
+//            }
+//            print("Success")
+//        }
 
 //        downloadJSON {
 //            self.profileName?.text = self.profile?.full_name
